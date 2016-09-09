eb6609d74a83c7a00d08a109645a84a0e8349ab2
Mikhail Pravilov <mikhail@pravilov.com>
2016-09-09 21:16:43 +0300

    All tasks are realised

---
+++ string_task.py
@@ -0,0 +1,74 @@
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
+#
+# Example input: 'read'
+# Example output: 'reading'
+
+
+def verbing(s):
+    if len(s) < 3:
+        return s
+    if s[-3::] == "ing":
+        s = s[:-3] + "ly"
+    else:
+        s += "ing"
+    return s
+
+
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+#
+# Example input: 'This dinner is not that bad!'
+# Example output: 'This dinner is good!'
+
+
+def not_bad(s):
+    n = s.find("not")
+    b = s.find("bad")
+    if n < b and n != -1:
+        s = s[:n] + "good" + s[b + 3:]
+    return s
+
+
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+#
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+#
+# Example input: 'abcd', 'xy'
+# Example output: 'abxcdy'
+
+
+def front_back(a, b):
+    l1 = len(a)
+    l2 = len(b)
+
+    if l1 % 2 != 0:
+        l1 = l1 / 2 + 1
+    else:
+        l1 /= 2
+    if l2 % 2 != 0:
+        l2 = l2 / 2 + 1
+    else:
+        l2 /= 2
+
+    return a[:l1] + b[:l2] + a[l1:] + b[l2:]
+
+
+def main():
+    s1, s2 = 'abcds', 'xyz'
+    s = front_back(s1, s2)
+    print(s)
+
+if __name__ == "__main__":
+    main()
---
+++ wordcount.py
@@ -0,0 +1,100 @@
+"""Wordcount exercise
+Google's Python class
+
+The main() below is already defined and complete. It calls print_words()
+and print_top() functions which you write.
+
+1. For the --count flag, implement a print_words(filename) function that counts
+how often each word appears in the text and prints:
+word1 count1
+word2 count2
+...
+
+Print the above list in order sorted by word (python will sort punctuation to
+come before letters -- that's fine). Store all the words as lowercase,
+so 'The' and 'the' count as the same word.
+
+2. For the --topcount flag, implement a print_top(filename) which is similar
+to print_words() but which prints just the top 20 most common words sorted
+so the most common word is first, then the next most common, and so on.
+
+Use str.split() (no arguments) to split on all whitespace.
+
+Workflow: don't build the whole program at once. Get it to an intermediate
+milestone and print your data structure and sys.exit(0).
+When that's working, try for the next milestone.
+
+Optional: define a helper function to avoid code duplication inside
+print_words() and print_top().
+
+"""
+
+import sys
+
+def read_words(filename):
+    words = []
+    with open(filename, "r") as f:
+        for line in f:
+            words.extend(line.split())
+    return words
+
+def get_words_dict(filename):
+    words = read_words(filename)
+    words_count = {}
+    for word in words:
+        add_flag = 0
+        if word.islower != 1:
+            word = word.lower()
+        for wordi in words_count:
+            if wordi == word:
+                words_count[wordi] += 1
+                add_flag = 1
+                break
+        if add_flag == 0:
+            words_count[word] = 1
+            add_flag = 1
+    return words_count
+
+def print_words(filename):
+    words_count = get_words_dict(filename)
+    words_count = sorted(words_count.items())
+    for word, num in words_count:
+        print(word + " " + str(num))
+
+def print_top(filename):
+    words_count = get_words_dict(filename)
+    words_count = sorted(words_count.items(), key=lambda x: x[1], reverse=True)
+    for i in range(20):
+        print(words_count[i][0])
+
+
+
+
+
+
+# Define print_words(filename) and print_top(filename) functions.
+# You could write a helper utility function that reads a file
+# and builds and returns a word/count dict for it.
+# Then print_words() and print_top() can just call the utility function.
+
+###
+
+# This basic command line argument parsing code is provided and
+# calls the print_words() and print_top() functions which you must define.
+def main():
+    if len(sys.argv) != 3:
+        print('usage: ./wordcount.py {--count | --topcount} file')
+        sys.exit(1)
+
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+        print_words(filename)
+    elif option == '--topcount':
+        print_top(filename)
+    else:
+        print('unknown option: ' + option)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
