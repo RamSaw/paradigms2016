0ae5d824fe292c5ba82eeb107ab0e6e5556e17d4
Mikhail Pravilov <mikhail@pravilov.com>
2016-09-25 17:43:28 +0300

    Corrected duplicates: rb mode, all paths are absolute,no exceptions if there is no argvs, pep8 corrected, file.close() deleted, dups.values() instead of dups.items()

--- T02Dups/FindDuplicate.py
+++ T02Dups/FindDuplicate.py
@@ -4,28 +4,27 @@ import hashlib
 
 
 def hash_file(path, blocksize=65536):
-    with open(path, 'r') as file:
+    with open(path, 'rb') as file:
         hash_sha = hashlib.sha1()
         buff = file.read(blocksize)
         while len(buff) > 0:
             hash_sha.update(buff)
             buff = file.read(blocksize)
-    file.close()
-    return hash_sha.hexdigest()
+        return hash_sha.hexdigest()
 
 
 def find_duplicates(top_dir):
     dups = {}
-    names = {}
-    for dirpath, dirnames, filenames in os.walk(top_dir):
+    names_to_hash = {}
+    for dirpath, _dirnames, filenames in os.walk(top_dir):
         filenames = filter(lambda x: not x.startswith(('.', '~')), filenames)
         for filename in filenames:
             path = os.path.join(dirpath, filename)
-            if filename in names:
-                dups[names[filename]] += ':' + path
+            if filename in names_to_hash:
+                dups[names_to_hash[filename]] += ':' + path
             else:
                 file_hash = hash_file(path)
-                names[filename] = file_hash
+                names_to_hash[filename] = file_hash
                 if file_hash in dups:
                     dups[file_hash] += ':' + path
                 else:
@@ -34,10 +33,11 @@ def find_duplicates(top_dir):
 
 
 def main():
-    dups = find_duplicates(sys.argv[1])
-    for hash, equals in dups.items():
-        if ':' in equals:
-            print(equals + '\n')
+    if len(sys.argv) > 1:
+        dups = find_duplicates(os.path.abspath(sys.argv[1]))
+        for equals in dups.values():
+            if ':' in equals:
+                print(equals)
 
 if __name__ == "__main__":
     main()
