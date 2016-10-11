2be49a85f18332d85617df9cd67a3b45d359f936
Mikhail Pravilov <mikhail@pravilov.com>
2016-10-11 21:35:45 +0300

    Eval problem corrected. Empty lists and read corrected.

--- T04Lang/model.py
+++ T04Lang/model.py
@@ -7,7 +7,7 @@ class Scope(object):
         self.d[key] = value
 
     def __getitem__(self, item):
-        if item not in self.d.keys():
+        if item not in self.d:
             return self.parent[item]
         return self.d[item]
 
@@ -52,13 +52,13 @@ class Conditional:
     def evaluate(self, scope):
         if self.condition.evaluate(scope).value:
             if not self.if_true:
-                return self.condition.evaluate(scope)
+                return Number(1)
             for true_command in self.if_true[:-1]:
                 true_command.evaluate(scope)
             return self.if_true[-1].evaluate(scope)
         else:
             if not self.if_false:
-                return self.condition.evaluate(scope)
+                return Number(1)
             for false_command in self.if_false[:-1]:
                 false_command.evaluate(scope)
             return self.if_false[-1].evaluate(scope)
@@ -79,7 +79,8 @@ class Read:
         self.name = name
 
     def evaluate(self, scope):
-        scope[self.name] = Number(input())
+        print("Input a number:")
+        scope[self.name] = Number(int(input()))
         return scope[self.name]
 
 
@@ -109,46 +110,36 @@ class BinaryOperation:
         self.lhs = lhs
         self.rhs = rhs
         self.op = op
+        self.operations = {'/': lambda x, y: x // y,
+                           '*': lambda x, y: x * y,
+                           '+': lambda x, y: x + y,
+                           '-': lambda x, y: x - y,
+                           '%': lambda x, y: x % y,
+                           '==': lambda x, y: int(x == y),
+                           '!=': lambda x, y: int(x != y),
+                           '<': lambda x, y: int(x < y),
+                           '>': lambda x, y: int(x > y),
+                           '<=': lambda x, y: int(x <= y),
+                           '>=': lambda x, y: int(x >= y),
+                           '&&': lambda x, y: x and y,
+                           '||': lambda x, y: x or y}
 
     def evaluate(self, scope):
-        operations = {'/': lambda x, y: Number(x.evaluate(scope).value
-                                               // y.evaluate(scope).value),
-                      '*': lambda x, y: Number(x.evaluate(scope).value
-                                               * y.evaluate(scope).value),
-                      '+': lambda x, y: Number(x.evaluate(scope).value
-                                               + y.evaluate(scope).value),
-                      '-': lambda x, y: Number(x.evaluate(scope).value
-                                               - y.evaluate(scope).value),
-                      '%': lambda x, y: Number(x.evaluate(scope).value
-                                               % y.evaluate(scope).value),
-                      '==': lambda x, y: Number(int(x.evaluate(scope).value
-                                                == y.evaluate(scope).value)),
-                      '!=': lambda x, y: Number(int(x.evaluate(scope).value
-                                                != y.evaluate(scope).value)),
-                      '<': lambda x, y: Number(int(x.evaluate(scope).value
-                                                   < y.evaluate(scope).value)),
-                      '>': lambda x, y: Number(int(x.evaluate(scope).value
-                                                   > y.evaluate(scope).value)),
-                      '<=': lambda x, y: Number(int(x.evaluate(scope).value
-                                                <= y.evaluate(scope).value)),
-                      '>=': lambda x, y: Number(int(x.evaluate(scope).value
-                                                >= y.evaluate(scope).value)),
-                      '&&': lambda x, y: Number(x.evaluate(scope).value
-                                                and y.evaluate(scope).value),
-                      '||': lambda x, y: Number(x.evaluate(scope).value
-                                                or y.evaluate(scope).value)}
-        return (operations[self.op])(self.lhs, self.rhs)
+        return Number((self.operations[self.op])
+                      (self.lhs.evaluate(scope).value,
+                       self.rhs.evaluate(scope).value))
 
 
 class UnaryOperation:
     def __init__(self, op, expr):
         self.op = op
         self.expr = expr
+        self.operations = {'-': lambda x: -x,
+                           '!': lambda x: int(not x)}
 
     def evaluate(self, scope):
-        operations = {'-': lambda x: Number(-x.evaluate(scope).value),
-                      '!': lambda x: Number(int(not x.evaluate(scope).value))}
-        return (operations[self.op])(self.expr)
+        return Number((self.operations[self.op])
+                      (self.expr.evaluate(scope).value))
 
 
 def example():
@@ -219,6 +210,12 @@ def my_tests():
     bin_opt = BinaryOperation(Number(10), '||', Number(2))
     res = bin_opt.evaluate(scope).value
     assert res != 0
+    bin_opt = BinaryOperation(Number(10), '==', Number(2))
+    res = bin_opt.evaluate(scope).value
+    assert res == 0
+    bin_opt = BinaryOperation(Number(10), '/', Number(2))
+    res = bin_opt.evaluate(scope).value
+    assert res == 5
 
     # # Test UnaryOperation class
     un_opt = UnaryOperation('!', Number(10))
@@ -226,14 +223,31 @@ def my_tests():
     assert res == 0
 
     # #Check empty condition and empty function
-    condition = Conditional(Number(2), [], [])
+    condition = Conditional(Number(0), None, [])
     res = condition.evaluate(scope)
-    assert res.value == 2
+    assert res.value == 1
+    condition = Conditional(Number(0), [], None)
+    res = condition.evaluate(scope)
+    assert res.value == 1
     function = Function([], [])
     res = function.evaluate(scope)
     assert res.value == 0
     print()
 
+    # #Check Read class, needed to int
+    read_test = Read('Read')
+    read_res = read_test.evaluate(scope)
+    condition = Conditional(Number(10), [Number(2), read_res], None)
+    res = condition.evaluate(scope)
+    assert res.value == read_res.value
+
+    # #Test scope doesn't rewrite values in parent scope
+    first_scope = Scope()
+    first_scope['num1'] = Number(1)
+    first_scope['num2'] = Number(2)
+    second_scope = Scope(first_scope)
+    second_scope['num1'] = Number(3)
+    assert second_scope['num1'] != first_scope['num1']
 
 if __name__ == '__main__':
     example()
